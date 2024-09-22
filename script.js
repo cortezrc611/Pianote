46d75dff40cc89664d3db86d73674fc363fbec5e
cortezrc611 <cortezrolly611@gmail.com>
2024-09-22 22:54:42 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburgerMenu = document.getElementById('hamburger-menu');
+    const sidebar = document.getElementById('sidebar');
+
+    hamburgerMenu.addEventListener('click', function() {
+        hamburgerMenu.classList.toggle('active');
+        sidebar.classList.toggle('active');
+    });
+});
